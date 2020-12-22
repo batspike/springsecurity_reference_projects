d8277ac4e387939fe6fcbc2a3b7ae91dd82b9e75
Sam Lau <samlau.sam@gmail.com>
2020-12-22 16:15:39 +0800

    project setup for jwt with DSL.

---
+++ OAuth2_DSL_JwtToken/OAuth2_DSL_JwtToken_AuthServer/src/main/java/com/samcancode/OAuth2DslJwtTokenAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.samcancode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2DslJwtTokenAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2DslJwtTokenAuthServerApplication.class, args);
+	}
+
+}
---
+++ OAuth2_DSL_JwtToken/OAuth2_DSL_JwtToken_ResourceServer/src/main/java/com/samcancode/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.samcancode.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello from Jwt Token DSL Resource Server!";
+	}
+}
