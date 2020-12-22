1722d41726238e3b0dadb647c63ec0bfa23f634e
Sam Lau <samlau.sam@gmail.com>
2020-12-22 13:49:52 +0800

    projects setup

---
+++ OAuth2_DSL_OpaqueToken/OAuth2_DSL_OpaqueToken_AuthServer/src/main/java/com/samcancode/OAuth2DslOpaqueTokenAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.samcancode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2DslOpaqueTokenAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2DslOpaqueTokenAuthServerApplication.class, args);
+	}
+
+}
